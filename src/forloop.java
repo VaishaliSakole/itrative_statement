5ebf13b3a971a31a8b4a2c0a019d663cc94c56f5
vaishali <vaishalisakole7@gmail.com>
2025-09-06 21:24:44 +0530

    third commit

---
+++ src/forloop.java
@@ -0,0 +1,29 @@
+
+public class forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int a=1;a<=10;a++) {
+	System.out.println("welcome");
+}
+for(int b=1;b<=10;b++) {
+	System.out.println("wel");
+}
+for(int i=0;i<5;i++) {
+	System.out.println(i);
+}
+for(int i=2;i<=20;i=i+2) {
+	System.out.println(i);
+}
+for(int i=10;i>=1;i--) {
+	System.out.println("hii");
+}
+//for(int a=100;a>=95;a++) {
+//	System.out.println("tka");
+//}
+//	
+
+}
+
+	}
+
